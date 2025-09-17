fc6c27c6b5f210490492a729a21b14bfebcee00b
岩塚 知季 <c332335@gmail.com>
2025-09-17 16:05:43 +0900

    feat(admin): split admin dashboard (/admin) and remove stats button from front

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
-
-import { HashRouter, Routes, Route } from 'react-router-dom'
 import AdminStats from './pages/AdminStats'
+
+// 最小のハッシュルーター
+function getHashPath() {
+  return window.location.hash.replace(/^#/, '') || '/'
+}
+function RootRouter() {
+  const [path, setPath] = React.useState(getHashPath())
+  React.useEffect(() => {
+    const onHash = () => setPath(getHashPath())
+    window.addEventListener('hashchange', onHash)
+    return () => window.removeEventListener('hashchange', onHash)
+  }, [])
+  if (path.startsWith('/admin')) return <AdminStats />
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminStats />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
+    <RootRouter />
+  </React.StrictMode>,
 )
