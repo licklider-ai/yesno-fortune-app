34f6db97a91d73186d80ce52d645c5949adeec34
岩塚 知季 <c332335@gmail.com>
2025-09-17 13:56:40 +0900

    chore(router): add /#/admin route for admin stats page

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
 
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import AdminStats from './pages/AdminStats'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminStats />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 )
